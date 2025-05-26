f024008ec919c1cba56382a35c9494cbed243265
kellerkatlin <kellerkatlin.k@gmail.com>
2025-05-26 15:12:59 -0500

    app

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://front-prueba-ecommerce-ayr-production.up.railway.app/',
+    origin: 'https://front-prueba-ecommerce-ayr-production.up.railway.app', // ✅ Sin "/"
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
   await app.listen(process.env.PORT ?? 3000);
 }
